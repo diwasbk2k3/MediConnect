b67595632cb2651746260ebf8b543df5a8d7609c
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-18 12:38:11 +0545

    Home Screen UI

--- lib/screens/bottom_screens/home_screen.dart
+++ lib/screens/bottom_screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mediconnect/screens/view_hospitals_screen.dart';
+import 'package:mediconnect/widgets/dashboard_card_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,8 +8,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
-      child: Center(
-        child: Text("WELCOME TO HOME SCREEN"),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            // View Hospitals Card
+            DashboardCard(
+              icon: 'assets/icons/view_hospital.png',
+              title: 'View Hospitals',
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ViewHospitalsScreen(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+      
+            // Book Appointments Card
+            DashboardCard(
+              icon: 'assets/icons/book_appointment.png',
+              title: 'Book Appointments',
+              onTap: () {},
+            ),
+            const SizedBox(height: 16),
+      
+            // View Reports Card
+            DashboardCard(
+              icon: 'assets/icons/health_report.png',
+              title: 'View Reports',
+              onTap: () {},
+            ),
+            const SizedBox(height: 16),
+      
+            // ChatBot Card
+            DashboardCard(
+              icon: 'assets/icons/chatbot.png',
+              title: 'ChatBot',
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
