5f37ed189858c3abeff68b954f76f2cdfc3b407b
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-03 22:38:43 +0545

    App Usecase

---
+++ lib/core/usecase/app_usecase.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:mediconnect/core/error/failures.dart';
+
+abstract interface class UseCaseWithParams<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
+
+abstract interface class UseCaseWithoutParams<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
