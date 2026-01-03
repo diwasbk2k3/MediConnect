af2e5906d5db10f0af6f5736385e5c74101060ec
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-03 21:32:36 +0545

    Hive Table Creation

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,10 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  // Database name
+  static const String dbName = "mediconnect_db";
+
+  // Hive Type IDs and Table Names
+  static const int authTypeId = 2;
+  static const String authTable = "auth_table";
+}
