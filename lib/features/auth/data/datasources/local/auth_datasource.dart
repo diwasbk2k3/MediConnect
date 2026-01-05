ff31f7bc7b895fb945ffad7e70b219c5c914ddbe
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-05 20:08:24 +0545

    Store user session data if login is successful

--- lib/features/auth/data/datasources/local/auth_datasource.dart
+++ lib/features/auth/data/datasources/local/auth_datasource.dart
@@ -1,19 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mediconnect/core/services/hive/hive_service.dart';
+import 'package:mediconnect/core/services/storage/user_session_service.dart';
 import 'package:mediconnect/features/auth/data/datasources/auth_datasource.dart';
 import 'package:mediconnect/features/auth/data/models/auth_hive_model.dart';
 
 // Provide implementation for Auth Local Datasource
 final authLocalDatasourceProvider = Provider<AuthLocalDatasource>((ref) {
-  final hiveService = ref.watch(hiveServiceProvider);
-  return AuthLocalDatasource(hiveService: hiveService);
+  final hiveService = ref.read(hiveServiceProvider);
+  final userSessionService = ref.read(userSessionServiceProvider);
+  return AuthLocalDatasource(
+    hiveService: hiveService,
+    userSessionService: userSessionService,
+  );
 });
 
 class AuthLocalDatasource implements IAuthDatasource {
   final HiveService _hiveService;
+  final UserSessionService _userSessionService;
 
-  AuthLocalDatasource({required HiveService hiveService})
-    : _hiveService = hiveService;
+  AuthLocalDatasource({
+    required HiveService hiveService,
+    required UserSessionService userSessionService,
+  }) : _hiveService = hiveService,
+       _userSessionService = userSessionService;
 
   @override
   Future<AuthHiveModel?> getCurrentUser() async {
@@ -37,7 +46,19 @@ class AuthLocalDatasource implements IAuthDatasource {
   Future<bool> login(String email, String password) async {
     try {
       final user = await _hiveService.loginUser(email, password);
-      return Future.value(user != null);
+      // Store session data if login is successful
+      if (user != null) {
+        await _userSessionService.storeUserSession(
+          isLoggedIn: true,
+          authId: user.authId!,
+          fullName: user.fullName,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          address: user.address,
+          profilePicture: user.profilePicture,
+        );
+      }
+      return user != null;
     } catch (e) {
       return Future.value(false);
     }
