52687c22d452f8e07ff01f74c07373d623ac48a4
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-15 17:00:28 +0545

    Auth Remote Data Source for register

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -150,6 +150,7 @@ class _AuthInterceptor extends Interceptor {
     // Skip auth for public endpoints
     final publicEndpoints = [
       ApiEndpoints.userLogin,
+      ApiEndpoints.userSignup
     ];
 
     final isPublicGet =
---
+++ lib/features/auth/data/datasources/remote/auth_remote_data_source.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mediconnect/core/api/api_client.dart';
+import 'package:mediconnect/core/api/api_endpoints.dart';
+import 'package:mediconnect/core/services/storage/user_session_service.dart';
+import 'package:mediconnect/features/auth/data/datasources/auth_datasource.dart';
+import 'package:mediconnect/features/auth/data/models/auth_api_model.dart';
+
+// Provider
+final authRemoteDatasoureProvider = Provider<IAuthRemoteDatasource>((ref) {
+  ;
+  return AuthRemoteDataSource(
+    apiClient: ref.read(apiClientProvider),
+    userSessionService: ref.read(userSessionServiceProvider),
+  );
+});
+
+class AuthRemoteDataSource implements IAuthRemoteDatasource {
+  final ApiClient _apiClient;
+  final UserSessionService _userSessionService;
+
+  AuthRemoteDataSource({
+    required ApiClient apiClient,
+    required UserSessionService userSessionService,
+  }) : _apiClient = apiClient,
+       _userSessionService = userSessionService;
+
+  @override
+  Future<AuthApiModel?> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthApiModel> login(String email, String password) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthApiModel> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthApiModel> register(AuthApiModel user) async {
+    final response = await _apiClient.post(
+      ApiEndpoints.userSignup,
+      data: user.toJson(),
+    );
+
+    if (response.data["success"] == true) {
+      final data = response.data["data"] as Map<String, dynamic>;
+      final registeredUser = AuthApiModel.fromJson(data);
+      return registeredUser;
+    }
+    return user;
+  }
+}
