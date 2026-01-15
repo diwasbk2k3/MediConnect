2d7dcfd99557b19e49fc8c026f2f4cca22555977
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-15 17:14:39 +0545

    Network Info

--- lib/features/auth/data/repositories/auth_repository.dart
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -14,9 +14,9 @@ final authRepositoryProvider = Provider<AuthRepository>((ref) {
 });
 
 class AuthRepository implements IAuthRepository {
-  final IAuthLocalDatasource _authDatasource;
+  final IAuthDatasource _authDatasource;
 
-  AuthRepository({required IAuthLocalDatasource authDatasource})
+  AuthRepository({required IAuthDatasource authDatasource})
     : _authDatasource = authDatasource;
 
   @override
