05b7f0190dfe14c6d0ea09633414d5613fd49da5
diwasbk2k3 <diwasbk2k3@gmail.com>
2026-01-26 12:49:49 +0545

    Configure API base URL for physical device, emulator, and web

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,12 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
 class ApiEndpoints {
   ApiEndpoints._();
 
-  // Base URL - change this for production
-  static const String baseUrl = 'http://10.0.2.2:4200/api';
-  //static const String baseUrl = 'http://localhost:3000/api';
-  // For Android Emulator use: 'http://10.0.2.2:3000/api'
-  // For iOS Simulator use: 'http://localhost:5000/api'
-  // For Physical Device use your computer's IP: 'http://192.168.x.x:5000/api'
+  static const bool isPhysicalDevice = true;
+
+  static const String compIpAddress = "192.168.18.6";
+
+  static String get baseUrl {
+    if (isPhysicalDevice) {
+      return 'http://$compIpAddress:4200/api';
+    }
+    // if android
+    if (kIsWeb) {
+      return 'http://localhost:4200/api';
+    } else if (Platform.isAndroid) {
+      return 'http://10.0.2.2:4200/api';
+    } else if (Platform.isIOS) {
+      return 'http://localhost:4200/api';
+    } else {
+      return 'http://localhost:4200/api';
+    }
+  }
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
